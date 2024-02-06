29e06f55ed73705325c551c9811aad8a5671e9a8
DennisChiira <programmingchiira@gmail.com>
2024-02-06 09:01:46 +0300

    vue starter

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is the home page</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
